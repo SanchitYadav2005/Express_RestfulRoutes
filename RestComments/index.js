3e1ad3360053af04f831ea2be08938e0e2f85d88
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-22 09:38:51 +0530

    added detail link to show detail of spefic id

--- RestComments/index.js
+++ RestComments/index.js
@@ -10,18 +10,22 @@ app.set('views', path.join(__dirname, '/views'))
 
 const comments = [
     {
+        id: 1,
         username : "Todd",
         comment : "nice I am loving it"
     },
     {
+        id:2,
         username : "John",
         comment : "delete your account otherwise ... "
     },
     {
+        id: 3,
         username : "Ron",
         comment : "nice car I am also thinking to buy it"
     },
     {
+        id: 4,
         username : "Shon",
         comment : "Hey buddy dinner tonight"
     },
@@ -40,6 +44,12 @@ app.post('/comments', (req,res)=>{
     comments.push({username, comment})
     //Redirects to the URL derived from the specified path, with specified status, a positive integer that corresponds to an HTTP status code . If not specified, status defaults to “302 “Found”.
     res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req,res)=>{
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === parseInt(id));
+    res.render('comments/show', {comment})
 })
 app.listen(port, function(){
     console.log(`listining on the port ${port}`);
