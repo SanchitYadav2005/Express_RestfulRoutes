942cb4167ff4d9997ffdfd510781201294c1c71e
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-22 13:52:17 +0530

    added a package that creates unique id's

--- RestComments/index.js
+++ RestComments/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); //generates unique id's
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
@@ -10,22 +12,22 @@ app.set('views', path.join(__dirname, '/views'))
 
 const comments = [
     {
-        id: 1,
+        id: uuidv4(),
         username : "Todd",
         comment : "nice I am loving it"
     },
     {
-        id:2,
+        id:uuidv4(),
         username : "John",
         comment : "delete your account otherwise ... "
     },
     {
-        id: 3,
+        id: uuidv4(),
         username : "Ron",
         comment : "nice car I am also thinking to buy it"
     },
     {
-        id: 4,
+        id: uuidv4(),
         username : "Shon",
         comment : "Hey buddy dinner tonight"
     },
@@ -41,16 +43,17 @@ app.get('/comments/new', (req, res)=>[
 ])
 app.post('/comments', (req,res)=>{
     const {username, comment} =req.body; 
-    comments.push({username, comment})
+    comments.push({username, comment, id : uuidv4()})
     //Redirects to the URL derived from the specified path, with specified status, a positive integer that corresponds to an HTTP status code . If not specified, status defaults to “302 “Found”.
     res.redirect('/comments');
 });
 
 app.get('/comments/:id', (req,res)=>{
     const {id} = req.params;
-    const comment = comments.find(c => c.id === parseInt(id));
+    const comment = comments.find(c => c.id === id);
     res.render('comments/show', {comment})
-})
+});
+
 app.listen(port, function(){
     console.log(`listining on the port ${port}`);
 })
