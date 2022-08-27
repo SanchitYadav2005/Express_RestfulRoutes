3405c9caf253809fa786c6dce2b09af5bff5ce3e
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-27 11:46:53 +0530

    added method overriding

--- StudentData/index.js
+++ StudentData/index.js
@@ -2,10 +2,25 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
+const methodOverriding = require("method-override");
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(methodOverriding('_method'));
+
+let studentdata = [
+    {
+        id: uuidv4(),
+        name: "Sanchit"
+    },
+    {
+        id: uuidv4(),
+        name: "Navneet"
+    }
+]
 
 app.get('/', (req, res)=>{
    res.render('home')
