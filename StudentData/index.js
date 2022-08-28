191ca1aab3f00b7f2604087644714c4d61586cfe
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-28 11:07:44 +0530

    fixed the issue

--- StudentData/index.js
+++ StudentData/index.js
@@ -15,63 +15,51 @@ app.use(express.json())
 
 let studentdata = [
     {
-        id: uuidv4(),
+        id: 1,
         student: "Sanchit",
         moto: "The purpose of our lives is to be happy."
     },
     {
-        id: uuidv4(),
+        id: 2,
         student: "Navneet",
         moto: "Life is what happens when you're busy making other plans."
     },
     {
-        id: uuidv4(),
+        id: 3,
         student: "Abhishek",
         moto: "Get busy living or get busy dying."
     },
     {
-        id: uuidv4(),
+        id: 4,
         student: "Rohit",
         moto:  "You only live once, but if you do it right, once is enough."
     },
     {
-        id: uuidv4(),
+        id: 5,
         student: "Nirdesh",
         moto:  "Many of life’s failures are people who did not realize how close they were to success when they gave up."
     }
 ]
 
-app.get('/', (req, res)=>{
+app.get('/student', (req, res)=>{
    res.render('home', {studentdata})
 });
 app.get('/new', (req, res)=>{
     res.render('new')
 });
-app.post('/', (req,res)=>{
+app.post('/student', (req,res)=>{
     const {student, moto} = req.body;
-    studentdata.push({student, moto, id: uuidv4()})
-    res.redirect('/');
-});
-app.get('/:id', (req, res)=>{
-    const {id} = req.params;
-    const student = studentdata.find(c => c.id == id);
-    res.render('show', {student})
-    console.log(req.params)
+    studentdata.push({student, moto})
+    res.redirect('/student');
 });
 
-app.get('/:id/edit', (req, res)=>{
+app.get('/student/:id', (req, res)=>{
     const {id} = req.params;
-    const student = studentdata.find(c => c.id == id);
-    res.render('edit', {student})
+    const student = studentdata.find(c => c.id === parseInt(id));
+    res.render('show', {student});
 });
 
-app.patch('/:id', (req, res)=>{
-    const {id} = req.params;
-    const newMoto = req.body.moto;
-    const foundid = studentdata.find(c => c.id == id);
-    foundid.moto = newMoto;
-    res.redirect('/');
-})
+
 
 app.listen(port, (err)=>{
     if(err){
