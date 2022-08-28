e1cea0e048c8ba3f5e2002931e00a34665a988bf
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-28 21:15:22 +0530

    done with post request and all things are working

--- todoApp/index.js
+++ todoApp/index.js
@@ -12,18 +12,22 @@ app.use(express.json());
 
 let todolist = [
     {
+        id:1,
         username: "Jon",
         task : "work on the login page."
     },
     {
+        id:2,
         username: "rizwan",
         task: "get the client list done."
     },
     {
+        id:3,
         username: "Sanchit",
         task: "deploy the landing page."
     },
     {
+        id:4,
         username: "Harry",
         task: "manage all the team members and their meetings"
     }
@@ -32,6 +36,16 @@ let todolist = [
 //getting the response on the home page
 app.get('/todo', (request, response)=>{
     response.render('home', {todolist});
+});
+// setting up a get request to get a form back for creating a new taks.
+app.get('/todo/new', (request, response)=>{
+    response.render('newTask')
+});
+// setting up post request to get the paramaters and add them to the array.
+app.post('/todo', (request, response)=>{
+    const {username, task} = request.body;
+    todolist.push({username,task, id: 5});
+    response.redirect('/todo');
 });
 
 app.listen(port, (err)=>{
